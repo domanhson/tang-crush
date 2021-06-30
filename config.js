930e55973df9f1280ba5efe4cb2feba30bc75583
domanhson <86732937+domanhson@users.noreply.github.com>
2021-06-30 22:52:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     introTitle: 'THẰNG BỎ MẸ BÁ ĐẠT !',
     introDesc: `MÀY CÓ ĐƯA TAO 1 TRIỆU KHÔNG???`,
-    btnIntro: 'ẤN VÀO ĐÂY TAO BẢO  ',
+    btnIntro: 'ẤN VÀO ĐÂY TAO BẢO',
     title: 'ĐƯA 1 TRIỆU ĐÂY',
     desc: 'HOẶC ĐỂ CÁNH TAY LẠI ',
-    btnYes: 'ANH ĐỌC SỐ TÀI KHOẢN EM ĐƯA Ạ !!! ',
+    btnYes: 'ANH ĐỌC SỐ TÀI KHOẢN EM ĐƯA Ạ !!!',
     btnNo: 'BỐ ĐÉO ĐƯA ',
-    question:'MÀY BIẾT SỐ TÀI KHOẢN TAO CHƯA???  ',
+    question:'MÀY BIẾT SỐ TÀI KHOẢN TAO CHƯA???',
     btnReply: 'NÓI GÌ ĐI',
-    reply: 'ANH SƠN ĐẸP TRAI 1013507985 VIETCOMBANK Ạ ',
-    mess: 'TỐT LẮM CON TRAI ',
-    messDesc: '2P NỮA CHUYỂN NHANH TAO ĐI MUA ĐÁ ',
-    btnAccept: 'OK EM CHUYỂN ANH ĐÂY Ạ 
-   ',
+    reply: 'ANH SƠN ĐẸP TRAI 1013507985 VIETCOMBANK Ạ',
+    mess: 'TỐT LẮM CON TRAI',
+    messDesc: '2P NỮA CHUYỂN NHANH TAO ĐI MUA ĐÁ',
+    btnAccept: 'OK EM CHUYỂN ANH ĐÂY Ạ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
