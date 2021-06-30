c092832c4f36e0ed6125dd29d82395d7d81f9a57
domanhson <86732937+domanhson@users.noreply.github.com>
2021-06-30 22:57:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'THẰNG CHÓ ĐẠT!',
+    introDesc: `MÀY LÀ CON CHÓ M CÓ CHO TAO 100 CỦ K`,
+    btnIntro: 'DẠ EM CÓ ',
+    title: '100 CỦ ',
+    desc: 'HOẶC ĂN ĐẤM ',
+    btnYes: 'EM HỨA SẼ ĐƯA ANH',
+    btnNo: 'BỐ ĐÉO ĐƯA',
+    question:'SAO MÀY LẠI CHO TAO',
+    btnReply: 'SỐ TÀI KHOẢN TAO',
+    reply: '1013507985 ANH SƠN ĐẸP TRAI VIETCOMBANK Ạ',
+    mess: 'GIỎI LẮM CON TRAI BỐ',
+    messDesc: 'CHUYỂN ANH K ĂN ĐẤM',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
